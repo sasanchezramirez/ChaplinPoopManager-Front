bdad9dba7c4ee8ce716c04be4e7706aa391c6653
santiago <sasanchezramirez@gmail.com>
2024-11-17 21:35:20 -0500

    feat: adding styles to dashboard

--- src/app/shared/components/heatmap/heatmap.component.ts
+++ src/app/shared/components/heatmap/heatmap.component.ts
@@ -18,7 +18,7 @@ export class HeatmapComponent {
 
   @Input() data: any[] = []; // Datos del mapa de calor
 
-  view: [number, number] = [600, 300]; // Ajusta el ancho y alto del gráfico
+  view: [number, number] = [800, 400];
   xAxis: boolean = true;
   yAxis: boolean = true;
   gradient: boolean = true;
@@ -26,6 +26,30 @@ export class HeatmapComponent {
     name: 'custom',
     selectable: true,
     group: ScaleType.Ordinal,
-    domain: ['#ffffb2', '#fed976', '#feb24c', '#fd8d3c', '#f03b20', '#bd0026']
+    domain: [
+      '#fee6ce', // Color más claro
+      '#fdd0a2',
+      '#fdae6b',
+      '#fd8d3c',
+      '#e6550d',
+      '#fb923c'  // Color de acento más intenso
+    ]
   };
+
+  ngOnInit() {
+    this.updateViewSize();
+    window.addEventListener('resize', this.updateViewSize.bind(this));
+  }
+
+  updateViewSize() {
+    const screenWidth = window.innerWidth;
+
+    // Para móviles
+    if (screenWidth <= 768) {
+      this.view = [screenWidth * 0.9, 300]; // 90% del ancho de la pantalla
+    } else {
+      // Para escritorio
+      this.view = [Math.min(screenWidth * 0.6, 800), 400]; // 60% del ancho o 800px máximo
+    }
+  }
 }
